909e35c53d5f65546d2340332934ab54a4e922bc
mihir-x <ictlancerx@gmail.com>
2023-12-21 00:17:09 +0600

    added mainlayout and home routes

---
+++ src/LayOuts/MainLayout.jsx
@@ -0,0 +1,13 @@
+import { Outlet } from "react-router-dom";
+
+
+const MainLayout = () => {
+    return (
+        <div>
+            this is main MainLayout
+            <Outlet></Outlet>
+        </div>
+    );
+};
+
+export default MainLayout;
