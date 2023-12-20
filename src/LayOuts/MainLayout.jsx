bf7ec471e149ba2baba76a057e1e95b919a26b0b
mihir-x <ictlancerx@gmail.com>
2023-12-21 00:40:00 +0600

    added navbar

--- src/LayOuts/MainLayout.jsx
+++ src/LayOuts/MainLayout.jsx
@@ -4,7 +4,6 @@ import { Outlet } from "react-router-dom";
 const MainLayout = () => {
     return (
         <div>
-            this is main MainLayout
             <Outlet></Outlet>
         </div>
     );
