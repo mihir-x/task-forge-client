a29c920b9d21196dc4d2d89e764deb4a46563cb9
mihir-x <ictlancerx@gmail.com>
2023-12-21 11:15:24 +0600

    added auth provider

---
+++ src/Provider/AuthProvider.jsx
@@ -0,0 +1,88 @@
+import { createContext, useEffect, useState } from "react";
+import { GoogleAuthProvider, createUserWithEmailAndPassword, onAuthStateChanged, signInWithEmailAndPassword, signInWithPopup, signOut, updateProfile } from "firebase/auth";
+import { auth } from "../Firebase/firebase.config";
+import PropTypes from 'prop-types'
+// import useAxiosPublic from "../Hooks/useAxiosPublic";
+
+export const AuthContext = createContext(null)
+const googleProvider = new GoogleAuthProvider()
+
+const AuthProvider = ({children}) => {
+
+    const [user, setUser] = useState(null)
+    const [loading, setLoading] = useState(true)
+    // const axiosPublic = useAxiosPublic()
+
+    const createUser = (email, password) =>{
+        setLoading(true)
+        return createUserWithEmailAndPassword(auth, email, password)
+    }
+
+    const logIn = (email, password) =>{
+        setLoading(true)
+        return signInWithEmailAndPassword(auth, email, password)
+    }
+
+    const googleSignIn = () =>{
+        setLoading(true)
+        return signInWithPopup(auth, googleProvider)
+    }
+
+    const logOut = () =>{
+        setLoading(true)
+        return signOut(auth)
+    }
+
+    const updateUserProfile = (name, photo) =>{
+        return updateProfile(auth.currentUser, {
+            displayName: name,
+            photoURL: photo,
+        })
+    }
+
+    // useEffect(()=>{
+    //     const unsubscribe = onAuthStateChanged(auth, currentUser =>{
+    //         const userEmail = currentUser?.email || user?.email
+    //         const loggedUser = {email: userEmail}
+    //         setUser(currentUser)
+    //         if(currentUser){
+    //             axiosPublic.post('/jwt', loggedUser, {withCredentials: true})
+    //             .then(res =>{
+    //                 console.log('token res: ', res.data)
+    //                 setLoading(false)
+    //             })
+    //         }
+    //         else{
+    //             axiosPublic.get('/jwt/logout', {withCredentials: true})
+    //             .then(res =>{
+    //                 console.log('token cleared: ', res.data)
+    //                 setLoading(false)
+    //             })
+    //         }
+    //     })
+    //     return () =>{
+    //         return unsubscribe()
+    //     }
+    // },[user?.email, axiosPublic])
+
+    useEffect(() => {
+        onAuthStateChanged(auth, currentUser => {
+            setUser(currentUser)
+            setLoading(false)
+        })
+    },[])
+
+    const authInfo = {
+        user, loading, createUser, logIn, googleSignIn, logOut, updateUserProfile,
+    }
+
+    return (
+        <AuthContext.Provider value={authInfo}>{children}</AuthContext.Provider>
+    );
+};
+
+export default AuthProvider;
+
+AuthProvider.propTypes = {
+    children: PropTypes.node
+}
--- src/main.jsx
+++ src/main.jsx
@@ -6,11 +6,14 @@ import {
 } from "react-router-dom";
 import { router } from './Routes/Routes';
 import { HelmetProvider } from 'react-helmet-async';
+import AuthProvider from './Provider/AuthProvider';
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
     <HelmetProvider>
-      <RouterProvider router={router} />
+      <AuthProvider>
+        <RouterProvider router={router} />
+      </AuthProvider>
     </HelmetProvider>
   </React.StrictMode>,
 )
